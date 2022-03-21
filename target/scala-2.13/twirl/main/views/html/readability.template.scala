1de8e565d7449c7f08da819f6c49b63218e1cb22
mahavir0 <170010116034@adit.ac.in>
2022-03-20 20:07:37 -0400

    added final changes

--- target/scala-2.13/twirl/main/views/html/readability.template.scala
+++ target/scala-2.13/twirl/main/views/html/readability.template.scala
@@ -40,15 +40,17 @@ Seq[Any](format.raw/*4.39*/("""
         <article style="margin-top:50px;">
             <div>
                 <h4>Readability for an Individual Project</h4>
-                """),_display_(/*12.18*/for( (keys,value) <- searchResults) yield /*12.53*/{_display_(Seq[Any](format.raw/*12.54*/("""
-                    """),_display_(/*13.22*/if(keys.equals("FleshIndex"))/*13.51*/{_display_(Seq[Any](format.raw/*13.52*/("""
-                        """),format.raw/*14.25*/("""<p>Flesch Readability Index: """),_display_(/*14.55*/value),format.raw/*14.60*/("""</p>
-                    """)))}),format.raw/*15.22*/("""
-                    """),_display_(/*16.22*/if(keys.equals("FKGL"))/*16.45*/{_display_(Seq[Any](format.raw/*16.46*/("""
-                        """),format.raw/*17.25*/("""<p>FKGL: """),_display_(/*17.35*/value),format.raw/*17.40*/("""</p>
-                    """)))}),format.raw/*18.22*/("""
-                """)))}),format.raw/*19.18*/("""
-            """),format.raw/*20.13*/("""</div>
+                <p>The Flesch–Kincaid readability tests are readability tests designed to indicate how difficult a passage in English is to understand. There are two tests: the Flesch Reading-Ease, and the Flesch–Kincaid Grade Level.</p>
+                <a href="https://en.wikipedia.org/wiki/Flesch%E2%80%93Kincaid_readability_tests">Click here to know more!</a>
+                """),_display_(/*14.18*/for( (keys,value) <- searchResults) yield /*14.53*/{_display_(Seq[Any](format.raw/*14.54*/("""
+                    """),_display_(/*15.22*/if(keys.equals("FleshIndex"))/*15.51*/{_display_(Seq[Any](format.raw/*15.52*/("""
+                        """),format.raw/*16.25*/("""<p>Flesch Readability Index: """),_display_(/*16.55*/value),format.raw/*16.60*/("""</p>
+                    """)))}),format.raw/*17.22*/("""
+                    """),_display_(/*18.22*/if(keys.equals("FKGL"))/*18.45*/{_display_(Seq[Any](format.raw/*18.46*/("""
+                        """),format.raw/*19.25*/("""<p>FKGL: """),_display_(/*19.35*/value),format.raw/*19.40*/("""</p>
+                    """)))}),format.raw/*20.22*/("""
+                """)))}),format.raw/*21.18*/("""
+            """),format.raw/*22.13*/("""</div>
         </article>
     </center>
 """)))}))
@@ -68,9 +70,9 @@ Seq[Any](format.raw/*4.39*/("""
               /*
                   -- GENERATED --
                   SOURCE: app/views/readability.scala.html
-                  HASH: ae955cb76bb7e339b068b20c3f52f43f1b3b585a
-                  MATRIX: 610->1|649->35|680->61|1020->80|1152->117|1184->124|1212->144|1251->146|1283->152|1464->306|1515->341|1554->342|1604->365|1642->394|1681->395|1735->421|1792->451|1818->456|1876->483|1926->506|1958->529|1997->530|2051->556|2088->566|2114->571|2172->598|2222->617|2264->631
-                  LINES: 23->1|24->2|25->3|30->4|35->4|38->7|38->7|38->7|39->8|43->12|43->12|43->12|44->13|44->13|44->13|45->14|45->14|45->14|46->15|47->16|47->16|47->16|48->17|48->17|48->17|49->18|50->19|51->20
+                  HASH: 841daba07222e9adf51baf607efb6f8a6a180e39
+                  MATRIX: 610->1|649->35|680->61|1020->80|1152->117|1184->124|1212->144|1251->146|1283->152|1830->672|1881->707|1920->708|1970->731|2008->760|2047->761|2101->787|2158->817|2184->822|2242->849|2292->872|2324->895|2363->896|2417->922|2454->932|2480->937|2538->964|2588->983|2630->997
+                  LINES: 23->1|24->2|25->3|30->4|35->4|38->7|38->7|38->7|39->8|45->14|45->14|45->14|46->15|46->15|46->15|47->16|47->16|47->16|48->17|49->18|49->18|49->18|50->19|50->19|50->19|51->20|52->21|53->22
                   -- GENERATED --
               */
           
